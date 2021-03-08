68459132e2ba7252e0cbe0e12517241b232c0707
Cledson F. Cavalcanti <cledsonitgames@gmail.com>
2021-03-08 12:07:01 -0300

    Criada a 'Imagem de disco'

---
+++ express/hello/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+// Posta arquivos do diretório 'public' na rota /media
+app.use('/media', express.static('public'))
+
+// Um exemplo de middleware
+function middleware_acs(req, res, next) {
+    console.log('O usuário está acessando "about"')
+    next()
+}
+
+app.use('/about', middleware_acs)
+
+// Instalação de rotas
+var wiki = require('./wiki.js')
+app.use(wiki)
+
+app.listen(3000, function() {
+  console.log('App de Exemplo escutando na porta 3000!');
+});
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+env PATH="$HOME/opt/nodejs-lts/bin:$PATH" codium
+
+# Um atalho para abrir o Codium em ambiente de desenvolvimento Node sem ter que configurar variáveis de ambiente de forma permanente
