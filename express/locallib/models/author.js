334a9332bfc36cf63420b903c866e1885b3ff1fa
Cledson F. Cavalcanti <cledsonitgames@gmail.com>
2021-03-18 19:47:43 -0300

    Desafio final da parte 5 de Express Tutorial

--- express/locallib/models/author.js
+++ express/locallib/models/author.js
@@ -23,7 +23,7 @@ AuthorSchema
 AuthorSchema
 .virtual('lifespan')
 .get(function() {
-    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString()
+    return this.date_of_birth_formatted + ' - ' + this.date_of_death_formatted
 })
 
 // Atributo virtual para a URL do autor
