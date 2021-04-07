d4a4b4e2a2952d3c90f0aba48fe21fe782eee255
Cledson F. Cavalcanti <cledsonitgames@gmail.com>
2021-04-07 11:47:11 -0300

    sem start.sh

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-PATH="$HOME/opt/nodejs-lts/bin:$PATH" codium
-
-# Um atalho para abrir o Codium em ambiente de desenvolvimento Node sem ter que configurar variáveis de ambiente de forma permanente
